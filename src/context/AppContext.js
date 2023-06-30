1a067bdc27424532d2f3753f8322dac9aa3d91ad
Dymas <ruannunes1999@gmail.com>
2023-06-29 21:58:55 -0300

    firebase login , tela inicial e carrinho

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -10,13 +10,12 @@ export const AppProvider = ({ children }) => {
   const login = async (email, password) => {
     try {
       const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
-      // O login foi bem-sucedido, você pode adicionar lógica adicional aqui se necessário
+     
       console.log('Login bem-sucedido:', userCredential.user);
-      setUser(userCredential.user); // Salva os dados do usuário no estado
+      setUser(userCredential.user); 
     } catch (error) {
-      // Ocorreu um erro durante o login, você pode tratar o erro aqui
       console.error('Erro no login:', error);
-      throw error; // Lança o erro para ser capturado no componente
+      throw error;
     }
   };
 
@@ -24,13 +23,12 @@ export const AppProvider = ({ children }) => {
     try {
       const provider = new firebase.auth.GoogleAuthProvider();
       const userCredential = await firebase.auth().signInWithPopup(provider);
-      // O login com o Google foi bem-sucedido, você pode adicionar lógica adicional aqui se necessário
       console.log('Login com o Google bem-sucedido:', userCredential.user);
-      setUser(userCredential.user); // Salva os dados do usuário no estado
+      setUser(userCredential.user); 
     } catch (error) {
-      // Ocorreu um erro durante o login com o Google, você pode tratar o erro aqui
+      
       console.error('Erro no login com o Google:', error);
-      throw error; // Lança o erro para ser capturado no componente
+      throw error;
     }
   };
 
